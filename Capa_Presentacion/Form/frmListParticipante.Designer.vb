10ea38e0755b1b876aa05b27527fdbe6eac6cd66
NonyGC <ngiraldocastillo@gmail.com>
2017-01-25 16:06:42 -0500

    LISTADO

--- Capa_Presentacion/Form/frmListParticipante.Designer.vb
+++ Capa_Presentacion/Form/frmListParticipante.Designer.vb
@@ -22,9 +22,23 @@ Partial Class FrmListParticipante
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Dim GridViewTextBoxColumn1 As Telerik.WinControls.UI.GridViewTextBoxColumn = New Telerik.WinControls.UI.GridViewTextBoxColumn()
+        Dim GridViewTextBoxColumn2 As Telerik.WinControls.UI.GridViewTextBoxColumn = New Telerik.WinControls.UI.GridViewTextBoxColumn()
+        Dim GridViewTextBoxColumn3 As Telerik.WinControls.UI.GridViewTextBoxColumn = New Telerik.WinControls.UI.GridViewTextBoxColumn()
+        Dim GridViewTextBoxColumn4 As Telerik.WinControls.UI.GridViewTextBoxColumn = New Telerik.WinControls.UI.GridViewTextBoxColumn()
+        Dim GridViewTextBoxColumn5 As Telerik.WinControls.UI.GridViewTextBoxColumn = New Telerik.WinControls.UI.GridViewTextBoxColumn()
+        Dim GridViewTextBoxColumn6 As Telerik.WinControls.UI.GridViewTextBoxColumn = New Telerik.WinControls.UI.GridViewTextBoxColumn()
+        Dim GridViewTextBoxColumn7 As Telerik.WinControls.UI.GridViewTextBoxColumn = New Telerik.WinControls.UI.GridViewTextBoxColumn()
+        Dim GridViewTextBoxColumn8 As Telerik.WinControls.UI.GridViewTextBoxColumn = New Telerik.WinControls.UI.GridViewTextBoxColumn()
+        Dim GridViewTextBoxColumn9 As Telerik.WinControls.UI.GridViewTextBoxColumn = New Telerik.WinControls.UI.GridViewTextBoxColumn()
+        Dim GridViewTextBoxColumn10 As Telerik.WinControls.UI.GridViewTextBoxColumn = New Telerik.WinControls.UI.GridViewTextBoxColumn()
+        Dim GridViewTextBoxColumn11 As Telerik.WinControls.UI.GridViewTextBoxColumn = New Telerik.WinControls.UI.GridViewTextBoxColumn()
+        Dim GridViewTextBoxColumn12 As Telerik.WinControls.UI.GridViewTextBoxColumn = New Telerik.WinControls.UI.GridViewTextBoxColumn()
+        Dim GridViewTextBoxColumn13 As Telerik.WinControls.UI.GridViewTextBoxColumn = New Telerik.WinControls.UI.GridViewTextBoxColumn()
         Dim TableViewDefinition1 As Telerik.WinControls.UI.TableViewDefinition = New Telerik.WinControls.UI.TableViewDefinition()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(FrmListParticipante))
         Me.RadGridView1 = New Telerik.WinControls.UI.RadGridView()
+        Me.Label15 = New System.Windows.Forms.Label()
         CType(Me.RadGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.RadGridView1.MasterTemplate, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me, System.ComponentModel.ISupportInitialize).BeginInit()
@@ -32,21 +46,75 @@ Partial Class FrmListParticipante
         '
         'RadGridView1
         '
-        Me.RadGridView1.Location = New System.Drawing.Point(25, 68)
+        Me.RadGridView1.Font = New System.Drawing.Font("Segoe UI", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RadGridView1.Location = New System.Drawing.Point(32, 45)
         '
         '
         '
+        Me.RadGridView1.MasterTemplate.AutoSizeColumnsMode = Telerik.WinControls.UI.GridViewAutoSizeColumnsMode.Fill
+        GridViewTextBoxColumn1.HeaderText = "CODIGO"
+        GridViewTextBoxColumn1.Name = "CODIGO"
+        GridViewTextBoxColumn1.Width = 74
+        GridViewTextBoxColumn2.HeaderText = "APE. PATERNO"
+        GridViewTextBoxColumn2.Name = "column1"
+        GridViewTextBoxColumn2.Width = 74
+        GridViewTextBoxColumn3.HeaderText = "APE. MATERNO"
+        GridViewTextBoxColumn3.Name = "column2"
+        GridViewTextBoxColumn3.Width = 74
+        GridViewTextBoxColumn4.HeaderText = "NOMBRES"
+        GridViewTextBoxColumn4.Name = "column3"
+        GridViewTextBoxColumn4.Width = 74
+        GridViewTextBoxColumn5.HeaderText = "SEXO"
+        GridViewTextBoxColumn5.Name = "column4"
+        GridViewTextBoxColumn5.Width = 74
+        GridViewTextBoxColumn6.HeaderText = "F. NACIMIENTO"
+        GridViewTextBoxColumn6.Name = "column5"
+        GridViewTextBoxColumn6.Width = 74
+        GridViewTextBoxColumn7.HeaderText = "DNI"
+        GridViewTextBoxColumn7.Name = "column6"
+        GridViewTextBoxColumn7.Width = 74
+        GridViewTextBoxColumn8.HeaderText = "DIRECCION"
+        GridViewTextBoxColumn8.Name = "column7"
+        GridViewTextBoxColumn8.Width = 74
+        GridViewTextBoxColumn9.HeaderText = "UBIGEO"
+        GridViewTextBoxColumn9.Name = "column8"
+        GridViewTextBoxColumn9.Width = 74
+        GridViewTextBoxColumn10.HeaderText = "FIJO"
+        GridViewTextBoxColumn10.Name = "column9"
+        GridViewTextBoxColumn10.Width = 74
+        GridViewTextBoxColumn11.HeaderText = "MOVIL"
+        GridViewTextBoxColumn11.Name = "column10"
+        GridViewTextBoxColumn11.Width = 74
+        GridViewTextBoxColumn12.HeaderText = "OP. MOVIL"
+        GridViewTextBoxColumn12.Name = "column11"
+        GridViewTextBoxColumn12.Width = 74
+        GridViewTextBoxColumn13.HeaderText = "EMAIL"
+        GridViewTextBoxColumn13.Name = "column12"
+        GridViewTextBoxColumn13.Width = 78
+        Me.RadGridView1.MasterTemplate.Columns.AddRange(New Telerik.WinControls.UI.GridViewDataColumn() {GridViewTextBoxColumn1, GridViewTextBoxColumn2, GridViewTextBoxColumn3, GridViewTextBoxColumn4, GridViewTextBoxColumn5, GridViewTextBoxColumn6, GridViewTextBoxColumn7, GridViewTextBoxColumn8, GridViewTextBoxColumn9, GridViewTextBoxColumn10, GridViewTextBoxColumn11, GridViewTextBoxColumn12, GridViewTextBoxColumn13})
         Me.RadGridView1.MasterTemplate.ViewDefinition = TableViewDefinition1
         Me.RadGridView1.Name = "RadGridView1"
-        Me.RadGridView1.Size = New System.Drawing.Size(732, 376)
+        Me.RadGridView1.Size = New System.Drawing.Size(973, 408)
         Me.RadGridView1.TabIndex = 0
         Me.RadGridView1.Text = "RadGridView1"
+        Me.RadGridView1.ThemeName = "VisualStudio2012Light"
+        '
+        'Label15
+        '
+        Me.Label15.Font = New System.Drawing.Font("Segoe UI", 12.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Underline), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label15.Location = New System.Drawing.Point(-4, -6)
+        Me.Label15.Name = "Label15"
+        Me.Label15.Size = New System.Drawing.Size(800, 21)
+        Me.Label15.TabIndex = 97
+        Me.Label15.Text = "LISTADO PARTICIPANTES REGISTRADOS"
+        Me.Label15.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
         '
         'FrmListParticipante
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(792, 489)
+        Me.ClientSize = New System.Drawing.Size(1049, 489)
+        Me.Controls.Add(Me.Label15)
         Me.Controls.Add(Me.RadGridView1)
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.Name = "FrmListParticipante"
@@ -64,5 +132,6 @@ Partial Class FrmListParticipante
     End Sub
 
     Friend WithEvents RadGridView1 As Telerik.WinControls.UI.RadGridView
+    Friend WithEvents Label15 As System.Windows.Forms.Label
 End Class
 
